46a865ee6f9129e69aa9d9f5e4a6e081b8686ea3
SotoDev0 <javier.soto8934@gmail.com>
2025-03-24 23:18:46 -0300

    first commit

---
+++ users/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,password=None,**extra_fields):
+        if email is None:
+            raise ValueError('the email es required')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+
+        return self.create_user(email,password,**extra_fields)
+    
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name','last_name']
+
+    objects = CustomUserManager()
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
