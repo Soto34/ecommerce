e4f0b225bcfe62e47a2a3d2a37a761bb5e88aa45
SotoDev0 <javier.soto8934@gmail.com>
2025-05-08 17:29:50 -0400

    se agregaron nuevos campos a producto

--- products/forms.py
+++ products/forms.py
@@ -4,15 +4,17 @@ from .models import Product, Category
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price', 'stock', 'category', 'image']
+        fields = ['codigo','name', 'description', 'price', 'stock','stock_min', 'category', 'image']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 3}),
         }
         labels = {
+            'codigo': 'Codigo',
             'name': 'Nombre del Producto',
             'description': 'Descripción',
             'price': 'Precio',
             'stock': 'Stock',
+            'stock_min' : 'Alerta de Stock',
             'category': 'Categoría',
             'image': 'Imagen'
         }
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -7,10 +8,12 @@ class Category(models.Model):
         return self.name
     
 class Product(models.Model):
+    codigo = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.IntegerField()
     stock = models.IntegerField()
+    stock_min = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/',null=True, blank=True)
 
